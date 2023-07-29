5698d0dbe7cd29891c556660e461b5b957333d5a
Theideabased <ogunmusireseyi01@gmail.com>
2023-07-29 08:57:32 -0700

    done

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,30 +1,30 @@
 #include "sort.h"
 
 /**
- * swap_int - this swap two integers in an array.
- * @a: is the first integer to swap.
- * @b: is the second integer to swap.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swap_int(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
-	int temp;
+	int tmp;
 
-	temp = *a;
+	tmp = *a;
 	*a = *b;
-	*b = temp;
+	*b = tmp;
 }
 
 /**
- * shell_sort - this sort an array of integers in ascending
+ * shell_sort - Sort an array of integers in ascending
  *              order using the shell sort algorithm.
- * @array: is the array of integers.
- * @size: is the size of the array.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
- * Description: this uses the Knuth interval sequence.
+ * Description: Uses the Knuth interval sequence.
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, j, k;
+	size_t gap, i, j;
 
 	if (array == NULL || size < 2)
 		return;
@@ -34,13 +34,13 @@ void shell_sort(int *array, size_t size)
 
 	for (; gap >= 1; gap /= 3)
 	{
-		for (j = gap; j < size; j++)
+		for (i = gap; i < size; i++)
 		{
-			k = j;
-			while (k >= gap && array[k - gap] > array[k])
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				swap_int(array + k, array + (k - gap));
-				k -= gap;
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
 			}
 		}
 		print_array(array, size);
